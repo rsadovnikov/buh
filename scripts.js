d298969313f91c4cda1d35f3dcea0e4f228dee10
RomanS <sdvnch@gmail.com>
2020-05-02 19:34:50 +1000

    jjj

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-var money = prompt("Братишка, сколько готов потратить?", "");
-    time = prompt("Теперь введи дату в формате YYYY-MM-DD", "");
+var money = prompt("Братишка, сколько готов потратить?", "300000");
+    time = prompt("Теперь введи дату в формате YYYY-MM-DD", "2016-02-07");
     
 var appData = {
     budget: money,
@@ -11,8 +11,8 @@ var appData = {
 };
 
 appData.expense.a = prompt("Введите обязательную статью расходов в этом месяце", "дрочить!");
-appData.expense.b = Number(prompt("Во сколько обойдется?", "30000"));
+appData.expense.b = prompt("Во сколько обойдется?", "30000");
 appData.expense.c = prompt("Введите обязательную статью расходов в этом месяце", "спать!");
-appData.expense.d = Number(prompt("Во сколько обойдется?", "30000"));
+appData.expense.d = prompt("Во сколько обойдется?", "30000");
 
-alert("В день будешь заносить " + (appData.expense.b + appData.expense.d)/30 + " ₽");
+alert(appData.budget /30 + " ₽");
