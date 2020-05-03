b86328e1c1d0c30b0212ebaa6600204e100121d2
RomanS <sdvnch@gmail.com>
2020-05-03 20:25:33 +1000

    циклы

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-var money = prompt("Братишка, сколько готов потратить?", "300000");
+let money = +prompt("Братишка, сколько готов потратить?", "120000");
     time = prompt("Теперь введи дату в формате YYYY-MM-DD", "2016-02-07");
     
-var appData = {
+let appData = {
     budget: money,
     timeData: time,
     expense : {},
@@ -10,9 +10,59 @@ var appData = {
     savings: false,
 };
 
-appData.expense.a = prompt("Введите обязательную статью расходов в этом месяце", "дрочить!");
-appData.expense.b = prompt("Во сколько обойдется?", "30000");
-appData.expense.c = prompt("Введите обязательную статью расходов в этом месяце", "спать!");
-appData.expense.d = prompt("Во сколько обойдется?", "30000");
+/* for (let i = 0; i < 2; i++) {
+    a = prompt("Введите обязательную статью расходов в этом месяце — " + (i + 1), "Похавать");
+    b = +prompt("Во сколько обойдётся?", "30000");
+    if (typeof(a) === "string" && typeof(a) != null && a != "" && typeof(b) === "number" && typeof(b) != null && b != "" 
+        && a.length < 50) {
+        appData.expense[a] = b;
+    }
+    else {
+        i--;
+        console.log("В данных косяк, повторяем цикл");
+    }
+    
+} */
 
-alert(appData.budget /30 + " ₽");
+let i = 0;
+/* while (i < 2) {
+    a = prompt("Введите обязательную статью расходов в этом месяце — " + (i + 1), "Похавать");
+    b = +prompt("Во сколько обойдётся?", "30000");
+    if (typeof(a) === "string" && typeof(a) != null && a != "" && typeof(b) === "number" && typeof(b) != null && b != "" 
+        && a.length < 50) {
+        appData.expense[a] = b;
+        i++;
+        console.log('всё ок');
+    }
+    else {
+        console.log("В данных косяк, повторяем цикл");
+    }
+} */
+
+/* do {
+    a = prompt("Введите обязательную статью расходов в этом месяце — " + (i + 1), "Похавать");
+    b = +prompt("Во сколько обойдётся?", "30000");
+    if (typeof(a) === "string" && typeof(a) != null && a != "" && typeof(b) === "number" && typeof(b) != null && b != "" 
+        && a.length < 50) {
+        appData.expense[a] = b;
+        i++;
+        console.log('всё ок');
+    }
+    else {
+        console.log("В данных косяк, повторяем цикл");
+    }
+} while (i < 2); */
+
+appData.moneyPerDay = appData.budget /30 ;
+
+alert("В день нужно " + appData.moneyPerDay + " ₽");
+
+if (appData.moneyPerDay < 100) {
+    console.log ("Нищеёб!");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Крепкий хозяйственник!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("При бабках!");
+} else {
+    console.log ("Чёт не то");
+}
